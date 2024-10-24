953fffb7e3c3957716208442fb8e404d0fef545f
Reem <Reem@DESKTOP-OQO8JHP>
2024-10-24 08:31:11 +0300

    add things in my project

--- Mango.Web/Controllers/CouponController.cs
+++ Mango.Web/Controllers/CouponController.cs
@@ -2,6 +2,7 @@
 using Mango.Web.Service.IService;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Mango.Web.Controllers
 {
@@ -18,5 +19,52 @@ namespace Mango.Web.Controllers
 
             return View(list);
         }
-    }
+        public async Task<IActionResult> CouponCreate()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseDto? response = await _couponService.CreateCouponAsync(model);
+                if (response != null && response.IsSuccess)
+                {
+                    TempData["success"] = "Coupon Created Successfully";
+                    return RedirectToAction(nameof(CouponIndex)); 
+                }
+                else
+                    TempData["error"] = response?.Message;    
+            }
+            return View(model);
+        }
+        public async Task<IActionResult> CouponDelete(int CouponId)
+        {
+			ResponseDto? response = await _couponService.GetCouponByIdAsync(CouponId);
+			if (response != null && response.IsSuccess)
+			{
+				CouponDto? coupon = JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(response.Result));
+				return View(coupon);
+			}
+            else
+                TempData["error"] = response?.Message;
+
+            return NotFound();
+        }
+        [HttpPost]
+		public async Task<IActionResult> CouponDelete(CouponDto model)
+		{
+			ResponseDto? response = await _couponService.DeleteCouponAsync(model.CouponId);
+			if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Coupon Deleted Successfully";
+                return RedirectToAction(nameof(CouponIndex));
+            }
+            else
+            TempData["error"] = response?.Message;
+            return View(model);
+		}
+	}
 }
