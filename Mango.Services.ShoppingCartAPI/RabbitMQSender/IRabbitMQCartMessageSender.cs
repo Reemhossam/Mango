6ba7fd0950986dc526606b50edc8095d8cafafb3
Reem <Reem@DESKTOP-OQO8JHP>
2024-10-31 08:18:07 +0300

    add things

---
+++ Mango.Services.ShoppingCartAPI/RabbitMQSender/IRabbitMQCartMessageSender.cs
@@ -0,0 +1,10 @@
+﻿
+using Mango.MessageBus;
+
+namespace Mango.Services.ShoppingCartAPI.RabbitMQSender
+{
+    public interface IRabbitMQCartMessageSender
+    {
+        void Send(BaseMessage message, string queueName);
+    }
+}
---
+++ Mango.Services.ShoppingCartAPI/RabbitMQSender/RabbitMQCartMessageSender.cs
@@ -0,0 +1,21 @@
+﻿using Mango.MessageBus;
+
+namespace Mango.Services.ShoppingCartAPI.RabbitMQSender
+{
+    public class RabbitMQCartMessageSender : IRabbitMQCartMessageSender
+    {
+        private readonly string _hostname;
+        private readonly string _username;
+        private readonly string _password;
+        public RabbitMQCartMessageSender()
+        {
+            _hostname = "localhost";
+            _hostname = "guest";
+            _username = "guest";
+        }
+        public void Send(BaseMessage message, string queueName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
